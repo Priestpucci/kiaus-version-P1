b31889e8d21141c0bb7e440b4e12a0a48f98b481
Priestpucci <46807@rws.uk.net>
2025-12-28 22:12:02 +0000

    new

--- src/modules/grade_calculator.py
+++ src/modules/grade_calculator.py
@@ -1,42 +1,57 @@
 #2025 A LEVEL grade Boundaries 
 grade_boundaries = []
 
+choice = input( "select method of calculation: (calculate Grade | 1 ) , (Calculate Percentage | 2) , (Calculate Grade & Percentage | 3) ")
+
 def Calculate_percentage(score):
   total_mark = float(input("Total Marks In Paper: "))
   P =  (round((score/total_mark) * 100))
-  print(f"Student percentage | {P}%")
+  return P
 
 def Grade(score):
   for i in range(6):
-      grade_boundaries.append(int(input(f"enter singular lower boundary in descending order of each grade ({i+1} value): "))) # Lower Boundaries: A*, A , B , C ,D ,E 
+    while True:
+        num = int(input(f"enter singular lower boundary in descending order of each grade value): "))# Lower Boundaries: A*, A , B , C ,D ,E 
 
-  print("Lower boundary of each grade | ", grade_boundaries)
+        if grade_boundaries and num >= grade_boundaries[-1]:
+           print("Invalid input | Lower grade boundary is greater than previous inputted value; try again.")
+    
+        else:
+          grade_boundaries.append(num)
 
+  print("Lower boundary of each grade | ", grade_boundaries)     
   if score >= grade_boundaries[0]:
-      print('student obtained | A*')
+        return 'A*'
   elif score >= grade_boundaries[1]:
-      print('student obtained | A')
+        return 'A'
   elif score >= grade_boundaries[2]:
-      print('student obtained | B')           # need to fix 
+        return 'B'          # need to fix 
   elif score >= grade_boundaries[3]:
-      print('student obtained | C')
+        return 'C'
   elif score >= grade_boundaries[4]:
-      print('student obtained | D')
+        return 'D'
   elif score >= grade_boundaries[5]:
-      print('student obtained | E')
+        return 'E'
   else:
-      print('student obtained | U')
+        return 'U'
          
 score = int(input("Enter the appropriate mark the student obtained out of Total: "))
-choice = input( "select method of calculation: (calculate Grade | 1 ) , (Calculate Percentage | 2) , (Calculate Grade & Percentage | 3) ")
 
 if choice == "1":
-    Grade(score)
+     Student_grade = Grade(score)
+     print(f'student obtained | {Student_grade}')
+
 elif choice == "2":
-    Calculate_percentage(score)
+     Student_percentage = Calculate_percentage(score)
+     print(f'student obtained | {Student_percentage}%')
+     
 elif choice == "3":
-    Grade(score) 
-    Calculate_percentage(score)
+     Student_grade = Grade(score)
+     Student_percentage = Calculate_percentage(score)
+     print (f'Student Percentage & Grade: {Student_percentage}% | {Student_grade}')
+    
+   
+     
     
 # NEED TO CREATE A  NAME SYSTEM; CAN ALLOCATE BOTH GRADES AND PERCENTAGES TO A STUDENT AND SAVE IT INTO A DATABASE
 # CSV FOR EXCEL DATABASE TO MAKE IT USER FRIENDLY AND ACCESIBLE 
