dcfe23e32e7dd296da9dc4f4e8773347047429e5
Priestpucci <46807@rws.uk.net>
2025-12-29 17:10:57 +0000

    hi

--- src/modules/grade_calculator.py
+++ src/modules/grade_calculator.py
@@ -1,7 +1,10 @@
 #2025 A LEVEL grade Boundaries 
+import csv
+
 grade_boundaries = []
 
 choice = input( "select method of calculation: (calculate Grade | 1 ) , (Calculate Percentage | 2) , (Calculate Grade & Percentage | 3) ")
+Student_name = input("Enter Student full name: ")
 
 def Calculate_percentage(score):
   total_mark = float(input("Total Marks In Paper: "))
@@ -10,15 +13,20 @@ def Calculate_percentage(score):
 
 def Grade(score):
   for i in range(6):
-    while True:
-        num = int(input(f"enter singular lower boundary in descending order of each grade value): "))# Lower Boundaries: A*, A , B , C ,D ,E 
+        while True:
+            try:
+                  num = int(input(f"enter singular lower boundary in descending order of each grade value): "))# Lower Boundaries: A*, A , B , C ,D ,E 
 
-        if grade_boundaries and num >= grade_boundaries[-1]:
-           print("Invalid input | Lower grade boundary is greater than previous inputted value; try again.")
+                  if grade_boundaries and num >= grade_boundaries[-1]:
+                       print("Invalid input | Lower grade boundary is greater than previous inputted value; try again.")
     
-        else:
-          grade_boundaries.append(num)
-
+                  else:
+                      grade_boundaries.append(num)
+                      break
+            
+            except ValueError:
+                 print('Invalid input, try again | Enter a valid interger ')
+   
   print("Lower boundary of each grade | ", grade_boundaries)     
   if score >= grade_boundaries[0]:
         return 'A*'
@@ -29,7 +37,6 @@ def Grade(score):
   elif score >= grade_boundaries[3]:
         return 'C'
   elif score >= grade_boundaries[4]:
-<<<<<<< HEAD
         return 'D'
   elif score >= grade_boundaries[5]:
         return 'E'
@@ -40,43 +47,28 @@ score = int(input("Enter the appropriate mark the student obtained out of Total:
 
 if choice == "1":
      Student_grade = Grade(score)
-     print(f'student obtained | {Student_grade}')
-=======
-      print('student obtained | B')
-  elif score >= grade_boundaries[6]:
-      print('student obtained | C')
-  elif score >= grade_boundaries[8]:
-      print('student obtained | D')')
-  elif score >= grade_boundaries[10]:
-      print('student obtained | E')
-  elif score >= grade_boundaries[11]:
-      print('student obtained | U'
-  else:
-      print('student obtained | F')
-         
-score = int(input("Enter the appropriate mark the student obtained out of Total: "))
-choice = input( "select method of calculation: (calculate Grade | 1 ) , (Calculate Percentage | 2) , (Calculate Grade & Percentage | 3) ")
-
-if choice == "1":
-    Grade(score)
-elif choice == "2":
-   Calculate_percentage(score)
-elif choice == "3":
-   grade(score) and 
->>>>>>> 32e628d8b1604075586071813f3ada4dae284c55
+     print(f'student {Student_name} 3obtained | {Student_grade}')
 
 elif choice == "2":
      Student_percentage = Calculate_percentage(score)
-     print(f'student obtained | {Student_percentage}%')
+     print(f'student {Student_name} obtained | {Student_percentage}%')
      
 elif choice == "3":
      Student_grade = Grade(score)
      Student_percentage = Calculate_percentage(score)
-     print (f'Student Percentage & Grade: {Student_percentage}% | {Student_grade}')
-    
-   
+     Student_info = (f'Student name | {Student_name}  | Student Percentage | {Student_percentage}% | Student grade | {Student_grade}')
+     print(Student_info)
+
+     with open ('Student_database.txt', 'w', newline="") as file:
+         writer = csv.writer(file)
+         writer.writerow(["Name" , "Percentage" , "Grade"])
+         writer.writerow(Student_info)
+
+
      
-    
+ 
+
+     
 # NEED TO CREATE A  NAME SYSTEM; CAN ALLOCATE BOTH GRADES AND PERCENTAGES TO A STUDENT AND SAVE IT INTO A DATABASE
 # CSV FOR EXCEL DATABASE TO MAKE IT USER FRIENDLY AND ACCESIBLE 
 #NEED TO BE ABLE TO DO BOTH FOR A STUDENT 
